79edc1689d4b4284821c2c335aa0a4db06f3f08c
Tiago Santos <tiagocsantos07@gmail.com>
2023-12-02 18:28:13 +0000

    Only T42 left to pass.

--- Projeto-1/Projeto1.cpp
+++ Projeto-1/Projeto1.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
-int maximizeValue(int plateColumn, int plateLine, std::vector<std::vector<int>> models,
-                  std::vector<int> values, int numTypes){
-    
-    std::vector<std::vector<int>>calculatedValues(plateColumn + 1, std::vector<int>(plateLine + 1, 0));
+int maximizeValue(int plateColumn, int plateLine, int numTypes, vector<vector<int>>calculatedValues){
+
     for(int c = 1; c <= plateColumn; c++){
         for(int l = 1; l <= plateLine; l++){
-            calculatedValues[c][l] = calculatedValues[c-1][l-1];
-            for(int m = 0; m < numTypes; m++){
-                int value1 = 0, value2 = 0, value3 = 0, value4 = 0, value5 = 0,
-                value6 = 0;
-                if(models[m][0] <= c && models[m][1] <= l){
-                    value3 = values[m] + calculatedValues[c - models[m][0]][models[m][1]] + calculatedValues[c][l - models[m][1]];
-                    value4 = values[m] + calculatedValues[models[m][0]][l - models[m][1]] + calculatedValues[c - models[m][0]][l];
-                    value6 = std::max(value3, value4);
+            for(int i = 1; i <= max(c,l)/2; i++){
+                int value1 = 0, value2 = 0;
+                if(i <= l){
+                    value1 = calculatedValues[c][i] + calculatedValues[c][l-i];
                 }
-                if(models[m][1] <= c && models[m][0] <= l){
-                    value1 = values[m] + calculatedValues[c - models[m][1]][models[m][0]] + calculatedValues[c][l-models[m][0]];
-                    value2 = values[m] + calculatedValues[c - models[m][1]][l] + calculatedValues[models[m][1]][l - models[m][0]];
-                    value5 = std::max(value1, value2);
+                if(i <= c){
+                    value2 = calculatedValues[i][l] + calculatedValues[c-i][l];
                 }
-                calculatedValues[c][l] = std::max(calculatedValues[c][l], std::max(value5, value6));
+                calculatedValues[c][l] = max(calculatedValues[c][l], max(value1, value2));
+                if(c <= plateLine && l <= plateColumn){
+                    calculatedValues[l][c] = calculatedValues[c][l];
+                }
             }
         }
     }
@@ -30,24 +26,24 @@ int maximizeValue(int plateColumn, int plateLine, std::vector<std::vector<int>> 
 
 int main(){
     int plateColumn = 0, plateLine = 0, numTypes = 0,
-    value = 0, maxValue = 0, line = 0, column = 0;
+    value = 0, line = 0, column = 0;
     
     std::cin >> plateColumn;
     std::cin >> plateLine;
     std::cin >> numTypes;
-
-    std::vector<std::vector<int>> models(numTypes, std::vector<int>(2, 0));
-    std::vector<int> values = std::vector<int>(numTypes, 0);
+    std::vector<std::vector<int>>calculatedValues(plateColumn + 1, std::vector<int>(plateLine + 1, 0));
 
     for(int i = 0; i < numTypes; i++){
         std::cin >> column;
         std::cin >> line;
         std::cin >> value;
-        models[i][0] = column;
-        models[i][1] = line;
-        values[i] = value;
+        if(column <= plateColumn && line <= plateLine){
+            calculatedValues[column][line] = value;
+        }
+        if(column <= plateLine && line <= plateColumn){
+            calculatedValues[line][column] = value;
+        }
     }
-    maxValue = maximizeValue(plateColumn, plateLine, models, values, numTypes);
-    std::cout << maxValue << std::endl;
+    std::cout << maximizeValue(plateColumn, plateLine, numTypes, calculatedValues) << std::endl;
     return 0;
 }
